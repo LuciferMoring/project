bb8c6127f1a1e422040f520440400e2728c4b0b8
LuciferMoring <275532734@qq.com>
2018-07-10 11:05:37 +0800

    changejs

--- shopping/js/banner.js
+++ shopping/js/banner.js
@@ -1,38 +1,79 @@
-var banner=(function(){
-	var $banner = $('.banner');
-	var $tips = $('.tips');
-	var $banner_img =$('.banner_img');
-	var timer=null;
+//var banner=(function(){
+//	var $banner = $('.banner');
+//	var $tips = $('.tips');
+//	var $banner_img =$('.banner_img');
+//	var timer=null;
+//	var index=0;
+//	return {
+//		init: function(){
+//			banner.tipsEvent();
+//			banner.autoPlay();
+//		},
+//		showImg:function(ind){
+//			$tips.children('li').eq(ind).addClass('active').siblings().removeClass('active');
+//			$banner_img.children('img').eq(ind).fadeIn().siblings().fadeOut();
+//		},
+//		tipsEvent:function(){
+//			$tips.children('li').on('click',function(){
+////				var index = $(this).index();
+//				banner.showImg(index);
+//				
+//			})
+//		},
+//		autoPlay:function(){
+//			clearInterval(timer);
+//			timer = setInterval(function(){
+//				banner.showImg(index);
+//				index++;
+//				if(index == 3){
+//					index=0;
+//				}
+//			},2000)
+//			
+//		}
+//
+//	}	
+//})()
+//banner.init();
+
+function Banner(){
+	this.banner = document.getElementsByClassName('banner')[0];
+	this.banner_img = this.banner.getElementsByClassName('banner_img')[0];
+	this.imgBox = this.banner_img.getElementsByTagName('img');
+	this.tips = this.banner.getElementsByClassName('tips')[0];
+	this.liBox = this.tips.getElementsByTagName('li');
+}
+Banner.prototype.swap=function(){
+	var _this=this;
 	var index=0;
-	return {
-		init: function(){
-			banner.tipsEvent();
-			banner.autoPlay();
-		},
-		showImg:function(ind){
-			$tips.children('li').eq(ind).addClass('active').siblings().removeClass('active');
-			$banner_img.children('img').eq(ind).fadeIn().siblings().fadeOut();
-		},
-		tipsEvent:function(){
-			$tips.children('li').on('click',function(){
-//				var index = $(this).index();
-				banner.showImg(index);
-				
-			})
-		},
-		autoPlay:function(){
-			clearInterval(timer);
-			timer = setInterval(function(){
-				banner.showImg(index);
-				index++;
-				if(index == 3){
-					index=0;
-				}
-			},2000)
-			
+	var timer=null;
+	function init(ind){
+		for(var i =0;i<_this.liBox.length;i++){
+			_this.liBox[i].className='';	
+			_this.imgBox[i].style.display="none";
 		}
-
-	}	
-})()
-banner.init();
-
+		_this.liBox[ind].className='active';
+		_this.imgBox[ind].style.display="block";
+	}
+	this.tips.onclick=function(e){
+		e = e || window.event;
+		var target = e.target || e.srcElement;
+		if(target.nodeName=="LI"){
+			autoPlay(target.innerHTML-1);
+			init(target.innerHTML-1);
+		}
+	}
+	function autoPlay(index){
+		clearInterval(timer);
+		timer =setInterval(function(){
+			index++;
+			if(index == _this.liBox.length){
+				index=0;
+			}
+			init(index);
+		},1000)
+	}
+	autoPlay(index);
+}
+var bannerBox = new Banner();
+bannerBox.swap();
---
+++ shopping/js/fangda.js
@@ -0,0 +1,66 @@
+function Bigmirror (){
+	this.pro_img = document.getElementsByClassName('pro_img')[0];
+	this.shadow = this.pro_img.getElementsByClassName('shadow')[0];
+	this.big_img = document.getElementsByClassName('big_img')[0];
+	this.pro_left = document.getElementsByClassName("pro_left")[0];
+	this.main = document.getElementsByClassName('main')[0];
+	this.imgBig = this.big_img.getElementsByTagName('img')[0];
+	this.tab_img = this.pro_left.getElementsByClassName('tab_img')[0]
+	this.tabImgs = this.tab_img.getElementsByTagName('img');
+	this.pro_img_img = this.pro_img.getElementsByTagName('img')[0];
+}
+Bigmirror.prototype.imgBoxmouseover=function(){
+	var _this=this;
+	this.pro_img.onmouseover=function(){
+		_this.shadow.style.display="block";
+	}
+	this.pro_img.onmouseout=function(){
+		_this.shadow.style.display="none";
+		_this.big_img.style.display="none";
+	}
+}
+Bigmirror.prototype.shadowmouseover=function(){
+	var _this=this;
+	this.shadow.onmousemove=function(e){
+		_this.big_img.style.display="block";
+		e = e || window.event;
+		var l = e.clientX - _this.main.offsetLeft - _this.shadow.offsetWidth/2;
+		var t = e.clientY - _this.pro_left.offsetTop - _this.shadow.offsetHeight/2;
+		var maxL = _this.pro_left.clientWidth - _this.shadow.offsetWidth;
+		var maxT = _this.pro_left.clientHeight - _this.shadow.offsetHeight;
+		if(l > maxL){
+			l = maxL;
+		}else if (l < 0 ){
+			l = 0;
+		}
+		if(t > maxT){
+			t =maxT;
+		}else if(t < 0){
+			t = 0;
+		}
+		_this.shadow.style.left = l + 'px';
+		_this.shadow.style.top = t + 'px';
+		_this.imgBig.style.left = -1*l + 'px';
+		_this.imgBig.style.top = -1*t + 'px';
+	}
+}
+Bigmirror.prototype.tabImg=function(){
+	var _this=this;
+//	console.log(this.pro_img);//大盒子
+//	console.log(this.tab_img);//小盒子
+//	console.log(this.pro_img_img)//大图
+//	console.log(this.tabImgs)//小图
+	this.tab_img.onclick=function(e){
+		e = e || window.event;
+		var target = e.target ||e.srcElement;
+		if(target.nodeName=='IMG'){
+			var imgSrc = target.src;
+			_this.pro_img_img.src = imgSrc.replace('small','big');
+			_this.imgBig.src = imgSrc.replace('small','big');
+		}
+	}
+}
+var imgBox = new Bigmirror();
+imgBox.imgBoxmouseover();
+imgBox.shadowmouseover();
+imgBox.tabImg();
